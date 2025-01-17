6601f0761b5d010fe73df920b061d8f9be9f94d4
PriyaGanjikunta <glakshmipriya987@gmail.com>
2025-01-17 13:26:09 +0530

    commit

--- rain.js
+++ rain.js
@@ -3,9 +3,9 @@ b1 = document.querySelector(".b1");
 b2 = document.querySelector(".b2");
 
 b1.addEventListener("click",() => {
-    im1.src="cat_rain.jpg";
+    im1.src="images/water.jpg";
 });
 
 b2.addEventListener("click",() => {
-    im1.src="cat_rain_stop.jpg";
+    im1.src="images/cool.jpg";
 });
